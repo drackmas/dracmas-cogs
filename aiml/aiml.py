01535c5618d2ed4a606eb0f235471bcac323f4b8
drackmas <62588681+drackmas@users.noreply.github.com>
2024-01-19 22:42:16 -0500

    Update aiml.py

--- aiml/aiml.py
+++ aiml/aiml.py
@@ -1,19 +1,18 @@
 import aiml
-import discord
 from redbot.core import commands
 
-class MentionCog(commands.Cog):
+class ChatBotCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.kernel = aiml.Kernel()
-        self.kernel.learn("std-startup.xml")
-        self.kernel.respond("load aiml b")
+        self.aiml_kernel = aiml.Kernel()
+        self.aiml_kernel.learn("std-startup.xml")
+        self.aiml_kernel.respond("load aiml b")
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if self.bot.user.mentioned_in(message):
-            response = self.kernel.respond(message.content)
+            response = self.aiml_kernel.respond(message.content)
             await message.channel.send(response)
 
 def setup(bot):
-    bot.add_cog(MentionCog(bot))
+    bot.add_cog(ChatBotCog(bot))
